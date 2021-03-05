b5b202126fe8c58182bbb51f41d7b002fd6b948f
Your Name <you@example.com>
2021-03-05 15:50:59 +0000

    first commit

---
+++ docker.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+s=sudo
+
+$s yum update -y
+if [ $? = 0 ];then
+  echo "update finished successfullu!"
+else
+  echo "update aborted! Something went wrong!"
+fi
+$s yum install -y yum-utils device-mapper-persistent-data lvm
+$s yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+$s yum-config-manager --enable docker-ce-edge
+$s yum-config-manager --enable docker-ce-test
+$s yum -y install docker-ce
+$s systemctl enable docker
+$s systemctl start docker
+$s curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+$s chmod +x /usr/local/bin/docker-compose
+$s ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+$s groupadd docker
+$s systemctl restart docker
+$s gpasswd -a $USER docker
+$s systemctl status docker
+docker-compose version
+if [ $? = 0 ]; then
+  echo "docker and docker-compose are available!"
+else
+  echo "something went wrong!"
+fi
